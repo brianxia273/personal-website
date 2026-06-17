ebc891028fb2b92c8460bf7623031931f39244dd
Brian Xia <briangxia@gmail.com>
2026-06-17 01:15:05 -0400

    analytics

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,12 @@
 import Image from "next/image";
 import HomePage from "@/app/Home/page";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function Home() {
-  return <HomePage />;
+  return (
+    <>
+      <HomePage />
+      <Analytics />
+    </>
+  );
 }
